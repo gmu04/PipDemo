c9c777304d321a2ae3bb86bb725c4036fd5dbe22
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2025-08-18 23:47:51 +0300

    Initial Commit

---
+++ PipDemo/PipDemoApp.swift
@@ -0,0 +1,13 @@
+//
+//  PipDemoApp.swift - created by Gokhan Mutlu on 18.08.2025
+
+import SwiftUI
+
+@main
+struct PipDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
