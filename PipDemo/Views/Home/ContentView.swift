a184be6a98e5a849a338a4796b59db1c8806467c
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2025-08-23 00:17:45 +0300

    add pip button

--- PipDemo/Views/Home/ContentView.swift
+++ PipDemo/Views/Home/ContentView.swift
@@ -2,6 +2,7 @@
 //  ContentView.swift - created by Gokhan Mutlu on 18.08.2025
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
     @StateObject var vm = ViewModel()
@@ -20,7 +21,8 @@ struct ContentView: View {
                     }
                     .frame(width: .infinity, height: 320, alignment: .center)
                     
-                    
+                    PipButtonView(vm)
+                        
                     
                     Divider()
                     
@@ -39,4 +41,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct PipButtonView: View {
+    let vm: ViewModel
+
+    @State private var isSelected:Bool = false
+
+    private let startImage = AVPictureInPictureController.pictureInPictureButtonStartImage
+    private let stopImage = AVPictureInPictureController.pictureInPictureButtonStopImage
+          
+    
+    init(_ vm:ViewModel){
+        self.vm = vm
+    }
+    var body: some View {
+        Button {
+            isSelected.toggle()
+        } label: {
+            Image(uiImage:(isSelected ? stopImage : startImage)
+                .withRenderingMode(.alwaysTemplate))
+                .resizable()
+                .frame(width: 50, height: 50)
+                .foregroundStyle(. blue)
+        }
+
+    }
 }
